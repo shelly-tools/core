5211e210db61b46576c3878ae22613e91ab2091a
Dominik Robert <dominikrobert@gmx.de>
2020-10-19 10:02:44 +0200

    Added Initial Vue Project

---
+++ ui/shelly-dashboard/src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+import './scss/styles.scss';
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount('#app');
