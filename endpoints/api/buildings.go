5e648ca16036a600fe60465224227037ed6ea759
Thorsten Eurich <thorsten@eurich.de>
2020-10-25 17:38:05 +0100

    some more work on buildings

--- endpoints/api/buildings.go
+++ endpoints/api/buildings.go
@@ -1,8 +1,8 @@
 package api
 
 import (
-	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
@@ -23,19 +23,52 @@ func GetAllBuildings(c *gin.Context) {
 	c.JSON(http.StatusOK, buildings)
 }
 
+// GetAllBuildings returns all buildings found in the database specified in the config
+func GetBuilding(c *gin.Context) {
+	var building models.Building
+	id := c.Param("id")
+	i, _ := strconv.Atoi(id)
+	err := common.DB.One("IDBuilding", i, &building)
+	if err != nil {
+		common.LogInstance.Errorf("Failed to get all buildings from the db:", err)
+	}
+
+	c.JSON(http.StatusOK, building)
+}
+
 // InsertOneBuidling inserts a building into the database
 func InsertOneBuilding(c *gin.Context) {
-	fmt.Println("hier")
+	var msg struct {
+		Status string `json:"status"`
+	}
 	var building models.Building
 	if err := c.ShouldBindBodyWith(&building, binding.JSON); err == nil {
-		c.String(http.StatusOK, "Body loaded", building.BuildingName)
+		msg.Status = "ok"
+		c.JSON(http.StatusOK, msg)
 
 		err := common.DB.Save(&building)
 		if err != nil {
 			common.LogInstance.Errorf("Failed to store building instance in database: %s", err)
 		}
-
 	} else {
-		c.String(http.StatusBadRequest, "JSON structire for building is wrong")
+		c.String(http.StatusBadRequest, "JSON structure for building is wrong")
+	}
+}
+
+func DeleteOneBuilding(c *gin.Context) {
+	var msg struct {
+		Status string `json:"status"`
+	}
+	var building models.Building
+	if err := c.ShouldBindBodyWith(&building, binding.JSON); err == nil {
+		msg.Status = "ok"
+		c.JSON(http.StatusOK, msg)
+
+		err := common.DB.DeleteStruct(&building)
+		if err != nil {
+			common.LogInstance.Errorf("Failed to store building instance in database: %s", err)
+		}
+	} else {
+		c.String(http.StatusBadRequest, "JSON structure for building is wrong")
 	}
 }
--- ui/assets/js/app.js
+++ ui/assets/js/app.js
@@ -24,6 +24,19 @@ function loadBuildings() {
         })
     });
 }
+function deleteBuilding(id) {
+  var tpldata;
+  fetch('buildings/delete_building.html',{cache: "no-store"})
+    .then((response) => response.text())
+    .then((template) => {
+      fetch('../api/v1/buildings/get/id/' + id )
+        .then(res => res.json())
+        .then(data => tpldata = data)
+        .then(() => {
+             document.getElementById('main').innerHTML = Mustache.render(template, tpldata);
+        })
+    });
+}
 
 function addBuilding() {
   var tpldata;
@@ -33,3 +46,40 @@ function addBuilding() {
         document.getElementById('main').innerHTML = Mustache.render(template);
     });
 }
+
+function createBuilding(opts) {
+  fetch('../api/v1/buildings/create', {
+    method: 'post',
+    body: JSON.stringify(opts)
+  }).then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    loadBuildings();    
+  });
+}
+function delBuilding(opts) {
+  fetch('../api/v1/buildings/delete/', {
+    method: 'post',
+    body: JSON.stringify(opts)
+  }).then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    loadBuildings();    
+  });
+}
+
+
+function submitBuilding() {
+  var content = document.querySelector('#buildingname').value;
+  if (content) {
+    createBuilding({"BuildingName" : content });
+  }
+ }
+
+ function submitDelBuilding(content) {
+  //var content = document.querySelector('#idbuilding').value;
+  if (content) {
+    delBuilding({"IDBuilding" : content });
+  }
+ }
+
