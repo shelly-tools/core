afcc6bd82ddb6a91be2e8381fda4aa766f4238bb
Thorsten Eurich <thorsten@eurich.de>
2020-10-26 17:16:44 +0100

    Added Model descriptions

--- models/rooms.go
+++ models/rooms.go
@@ -2,10 +2,10 @@ package models
 
 // Room inside a building
 type Room struct {
-	IDRoom          int    `storm:"id,unique,increment"`
-	RoomName        string `storm:"index"`
-	IDBuilding      int
-	RoomOrder       int
-	RoomPicture     string
-	IDDefaultSensor int
+	IDRoom          int    `storm:"id,unique,increment"` // the uniqe ID of the room
+	RoomName        string `storm:"index"`               //the Rooms name
+	IDBuilding      int    // the ID of the building the room was placed in
+	RoomOrder       int    // the order number, 1 is shown first in the list of rooms
+	RoomPicture     string // a picture from the room
+	IDDefaultSensor int    // the id of a temperature (and humidity) sensor, temperature wiil be shown in room index
 }
--- models/users.go
+++ models/users.go
@@ -2,11 +2,11 @@ package models
 
 // User represents a User for the UI or the API
 type User struct {
-	IDUser   int    `storm:"id,unique"`
-	Username string `storm:"index"`
-	Surname  string
-	IDGroup  int
-	Name     string
-	EMail    string
-	Password string
+	IDUser        int    `storm:"id,unique"` //unique member ID
+	Username      string `storm:"index"`     // username, will be used for login
+	Surname       string // the users surname
+	IDMemberGroup int    // the ID of the primary membergroup the user belongs to
+	Name          string // the users last name
+	EMail         string // the users E-Mail address
+	Password      string // the password
 }
